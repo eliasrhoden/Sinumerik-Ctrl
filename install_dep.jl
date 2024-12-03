5e30619d18d3b8ea4a84439a4995d5054de40bc9
Elias <elias.rhoden@hotmail.com>
2024-12-03 22:19:57 +0100

    initial

---
+++ install_dep.jl
@@ -0,0 +1,10 @@
+import Pkg; Pkg.add("ControlSystemsBase")
+import Pkg; Pkg.add("RobustAndOptimalControl")
+import Pkg; Pkg.add("Plots")
+
+
+using RobustAndOptimalControl
+using ControlSystemsBase 
+using Plots
+
+print("Install ok \n")
